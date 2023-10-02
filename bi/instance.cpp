9aa8e2c68b0728e16fe50b7007ec3c4e36764e5e
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2023-10-02 22:07:14 +0200

    .gitignore is now working

--- bi/instance.cpp
+++
@@ -1,9 +0,0 @@
-#include "helpers/shortguid.h"
-#include "instance.h"
-
-const QString Instance::_value = GetValue();
-
-QString Instance::GetValue()
-{
-    return zShortGuid::createNew().value;
-}
